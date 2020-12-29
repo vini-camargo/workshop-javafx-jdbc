a8681f80bbb1caf2f387d29db7f43f2781af3fff
Vinicius Camargo <vicamargofranca@gmail.com>
2020-12-29 12:52:30 -0300

    Main view

--- src/module-info.java
+++ src/module-info.java
@@ -1,5 +1,11 @@
 module workshopJavafxJdbc {
 	requires javafx.controls;
+	requires javafx.base;
+	requires javafx.fxml;
+	requires javafx.graphics;
+	requires javafx.media;
+	requires javafx.swing;
+	requires javafx.web;
 	
 	opens application to javafx.graphics, javafx.fxml;
 }
